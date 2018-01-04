e401a135a0ee28e2b79b8ce536747a34bcb86c1b
zhvaniaz <35070821+zhvaniaz@users.noreply.github.com>
2018-01-04 17:51:24 +0400

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var pre = document.querySelectorAll("pre");
+var colors = ["red","red","orange","yellow","chartreuse","cyan","violet","purple","purple","indigo"];
+var tempColors = [];
+
+
+setInterval(function(){
+  
+for (i=0; i < pre.length; i++)
+  {
+    if (i == pre.length-1)
+      {
+        tempColors[i] = colors[0];
+        colors = tempColors;
+      }
+    else
+      {
+        tempColors[i] = colors[i+1];
+      }
+    pre[i].style.color = tempColors[i];
+  };
+
+},60);
